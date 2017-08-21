f2a53028a28682aa52278b7ac7515989683cd646
tachisis <engel.t@gmail.com>
2017-08-21 18:18:17 +0300

    Fix missing js/

---
+++ js/pictures.js
@@ -0,0 +1,109 @@
+'use strict';
+
+var COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+var photoTemplate = document
+    .querySelector('#picture-template')
+    .content
+    .querySelector('.picture');
+
+function getRandom(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function shuffle(array) {
+  var current = array.length;
+  var tmp;
+  var random;
+
+  while (current--) {
+    random = Math.floor(Math.random() * current);
+    tmp = array[current];
+    array[current] = array[random];
+    array[random] = tmp;
+  }
+
+  return array;
+}
+
+function getUrls(count) {
+  var urls = [];
+
+  for (var i = 0; i < count; i++) {
+    urls[i] = i + 1;
+  }
+
+  return urls;
+}
+
+function getRandomComments(comments) {
+  var shuffledComments = shuffle(comments);
+  var randomCount = getRandom(1, 2).toFixed();
+
+  comments = [];
+
+  for (var i = 0; i < randomCount; i++) {
+    comments[i] = shuffledComments[i];
+  }
+
+  return comments;
+}
+
+function getPhotos(comments, count) {
+  var shuffledUrls = shuffle(getUrls(count));
+
+  var photos = [];
+
+  for (var i = 0; i < count; i++) {
+    photos[i] = {
+      url: 'photos/' + shuffledUrls[i] + '.jpg',
+      likes: getRandom(15, 200).toFixed(),
+      comments: getRandomComments(comments).length
+    };
+  }
+
+  return photos;
+}
+
+function getPhotoElement(photos) {
+  var photo = photoTemplate.cloneNode(true);
+
+  photo.querySelector('.picture > img').src = photos.url;
+  photo.querySelector('.picture-likes').textContent = photos.likes;
+  photo.querySelector('.picture-comments').textContent = photos.comments;
+
+  return photo;
+}
+
+function renderPhotos(photos, target) {
+  var photosListElement = document.querySelector(target);
+  var fragment = document.createDocumentFragment();
+  var count = photos.length;
+
+  for (var i = 0; i < count; i++) {
+    fragment.appendChild(getPhotoElement(photos[i]));
+  }
+
+  photosListElement.appendChild(fragment);
+}
+
+function showPhotoOverlay(photo) {
+  var photoOverlay = document.querySelector('.gallery-overlay');
+  photoOverlay.querySelector('.gallery-overlay-image').src = photo.url;
+  photoOverlay.querySelector('.likes-count').textContent = photo.likes;
+  photoOverlay.querySelector('.comments-count').textContent = photo.comments;
+  photoOverlay.classList.remove('hidden');
+}
+
+var photos = getPhotos(COMMENTS, 25);
+renderPhotos(photos, '.pictures');
+
+document.querySelector('.upload-overlay').classList.add('hidden');
+showPhotoOverlay(photos[0]);
